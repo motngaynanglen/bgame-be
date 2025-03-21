0f66a8c2ada116e3d9d64e0abea1f722cebc46a7
PhamPhucThien <sherrin1st@gmail.com>
2025-03-21 18:17:03 +0700

    Auto stash before merge of "Thien" and "master"

---
+++ BG_IMPACT/Extensions/HttpContextExtension.cs
@@ -0,0 +1,10 @@
+﻿namespace BG_IMPACT.Extensions
+{
+    public static class HttpContextExtension
+    {
+        public static string GetName(this HttpContext context)
+        {
+            return context.User?.Claims?.SingleOrDefault(p => p.Type.Contains("name"))?.Value ?? string.Empty;
+        }
+    }
+}
