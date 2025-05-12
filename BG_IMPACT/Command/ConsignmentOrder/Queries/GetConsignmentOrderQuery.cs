457ed3e62a197bb966ae9e346cb2cbef67b435d0
bachdoan-sa <bach.doansy@gmail.com>
2025-05-12 17:22:10 +0700

    add ConsignmentOrder repository, command, query

---
+++ BG_IMPACT/Command/ConsignmentOrder/Queries/GetConsignmentOrderQuery.cs
@@ -0,0 +1,90 @@
+﻿using BG_IMPACT.Extensions;
+using BG_IMPACT.Models;
+using BG_IMPACT.Repositories.Implementations;
+using BG_IMPACT.Repositories.Interfaces;
+using MediatR;
+
+namespace BG_IMPACT.Command.BookList.Queries
+{
+    public class GetConsignmentOrderQuery : IRequest<ResponseObject>
+    {
+
+        public Paging Paging { get; set; } = new Paging();
+
+        public class GetConsignmentOrderHandler : IRequestHandler<GetConsignmentOrderQuery, ResponseObject>
+        {
+            private readonly IConsignmentOrderRepository _consignmentOrderRepository;
+            private readonly IHttpContextAccessor _httpContextAccessor;
+
+            public GetConsignmentOrderHandler(IConsignmentOrderRepository consignmentOrderRepository, IHttpContextAccessor httpContextAccessor)
+            {
+                _consignmentOrderRepository = consignmentOrderRepository;
+                _httpContextAccessor = httpContextAccessor;
+            }
+            public async Task<ResponseObject> Handle(GetConsignmentOrderQuery request, CancellationToken cancellationToken)
+            {
+                ResponseObject response = new();
+                var context = _httpContextAccessor.HttpContext;
+
+                string? UserID = context?.GetName() ?? null;
+                string? Role = context?.GetRole() ?? null;
+
+                if (Role == null || Guid.TryParse(UserID, out _) == false)
+                {
+                    response.StatusCode = "404";
+                    response.Message = "Token trả về không đúng.";
+                }
+                else
+                {
+                    object param = new
+                    {
+                        UserID,
+                        Role,
+                        request.Paging.PageNum,
+                        request.Paging.PageSize
+                    };
+
+                    object param2 = new
+                    {
+                        UserID,
+                        Role
+                    };
+
+                    var result = await _consignmentOrderRepository.spConsignmentOrderGetList(param);
+                    var list = ((IEnumerable<dynamic>)result).ToList();
+
+                    //var pageData = await _bookListRepository.spBookListGetPageData(param2);
+                    //var dict = pageData as IDictionary<string, object>;
+                    long count = 0;
+
+                    //if (dict != null && Int64.TryParse(dict["TotalRows"].ToString(), out _) == true)
+                    //{
+                    //    _ = Int64.TryParse(dict["TotalRows"].ToString(), out count);
+                    //}
+
+                    if (list.Count > 0)
+                    {
+                        //long pageCount = count / request.Paging.PageSize;
+
+                        response.StatusCode = "200";
+                        response.Data = list;
+                        response.Message = string.Empty;
+                        //response.Paging = new PagingModel
+                        //{
+                        //    PageNum = request.Paging.PageNum,
+                        //    PageSize = request.Paging.PageSize,
+                        //    PageCount = count % request.Paging.PageSize == 0 ? pageCount : pageCount + 1
+                        //};
+                    }
+                    else
+                    {
+                        response.StatusCode = "404";
+                        response.Message = "Không tìm thấy đơn thuê nào.";
+                    }
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ BG_IMPACT/Repositories/Implementations/ConsignmentOrderRepository.cs
@@ -0,0 +1,47 @@
+﻿using BG_IMPACT.Repositories.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace BG_IMPACT.Repositories.Implementations
+{
+    public class ConsignmentOrderRepository : IConsignmentOrderRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public ConsignmentOrderRepository(SqlConnection sqlConnection)
+        {
+            _connection = sqlConnection;
+
+            if (_connection.State != ConnectionState.Open)
+            {
+                _connection.OpenAsync().GetAwaiter().GetResult();
+            }
+        }
+        public async Task<object?> spConsignmentOrderGetById(object param)
+        {
+            object? result = await _connection.QueryFirstOrDefaultAsync("spConsignmentOrderGetById", param, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+        public async Task<object?> spConsignmentOrderGetList(object param)
+        {
+            object? result = await _connection.QueryAsync("spConsignmentOrderGetList", param, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+        public async Task<object?> spConsignmentOrderCancelByStaff(object param)
+        {
+            object? result = await _connection.QueryFirstOrDefaultAsync("spConsignmentOrderCancelByStaff", param, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+        public async Task<object?> spConsignmentOrderCreateByStaff(object param)
+        {
+            object? result = await _connection.QueryFirstOrDefaultAsync("spConsignmentOrderCreateByStaff", param, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+        public async Task<object?> spConsignmentOrderUpdateByStaff(object param)
+        {
+            object? result = await _connection.QueryFirstOrDefaultAsync("spConsignmentOrderUpdateByStaff", param, commandType: CommandType.StoredProcedure);
+            return result;
+        }
+    }
+}
