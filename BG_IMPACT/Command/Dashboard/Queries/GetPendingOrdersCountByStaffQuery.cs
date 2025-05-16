189c9f054b3965f8c290ecb92fcde15ab294bfb5
vmtn2210 <vmtan2210@gmail.com>
2025-05-16 17:56:08 +0700

    Dashboard

---
+++ BG_IMPACT/Command/Dashboard/Queries/GetPendingOrdersCountByStaffQuery.cs
@@ -0,0 +1,52 @@
+﻿using BG_IMPACT.Extensions;
+using BG_IMPACT.Models;
+using BG_IMPACT.Repositories.Interfaces;
+using MediatR;
+
+namespace BG_IMPACT.Command.Dashboard.Queries
+{
+    public class GetPendingOrdersCountByStaffQuery : IRequest<ResponseObject>
+    {
+        public class GetPendingOrdersCountByStaffQueryHandler : IRequestHandler<GetPendingOrdersCountByStaffQuery, ResponseObject>
+        {
+            private readonly IDashboardRepository _dashboardRepository;
+            private readonly IHttpContextAccessor _httpContextAccessor;
+
+            public GetPendingOrdersCountByStaffQueryHandler(IDashboardRepository dashboardRepository, IHttpContextAccessor httpContextAccessor)
+            {
+                _dashboardRepository = dashboardRepository;
+                _httpContextAccessor = httpContextAccessor;
+            }
+            public async Task<ResponseObject> Handle(GetPendingOrdersCountByStaffQuery request, CancellationToken cancellationToken)
+            {
+                ResponseObject response = new();
+                var context = _httpContextAccessor.HttpContext;
+
+                string? UserID = context?.GetName() ?? null;
+
+                object param = new
+                {
+                    UserID
+                };
+
+                var result = await _dashboardRepository.spGetPendingOrdersCountByStaff(param);
+                var dict = result as IDictionary<string, object>;
+
+
+                if (dict != null && dict.Count > 0)
+                {
+                    response.StatusCode = "200";
+                    response.Data = dict;
+                    response.Message = string.Empty;
+                }
+                else
+                {
+                    response.StatusCode = "404";
+                    response.Message = "Không tìm thông tin.";
+                }
+
+                return response;
+            }
+        }
+    }
+}
---
+++ BG_IMPACT/Repositories/Interfaces/IDashboardRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace BG_IMPACT.Repositories.Interfaces
+{
+    public interface IDashboardRepository
+    {
+        Task<object?> spGetTodayOrderRevenueByStaff(object param);
+        Task<object?> spGetTodayBookListRevenueByStaff(object param);
+        Task<object?> spGetPendingOrdersCountByStaff(object param);
+        Task<object?> spGetTodayActiveOrdersByStaff(object param);
+        Task<object?> spGetPendingBookListCountToday(object param);
+    }
+}
