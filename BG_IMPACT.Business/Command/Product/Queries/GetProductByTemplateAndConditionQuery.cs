901fb1636ebca5ee257ebe4f3d36bf140167f521
PhamPhucThien <sherrin1st@gmail.com>
2025-06-05 14:55:54 +0700

    update transaction offline and online

--- BG_IMPACT.Business/Command/Product/Queries/GetProductByTemplateAndConditionQuery.cs
+++ BG_IMPACT.Business/Command/Product/Queries/GetProductByTemplateAndConditionQuery.cs
@@ -27,16 +27,42 @@
                     request.TemplateID,
                     UserID,
                     request.ConditionFilter,
+                    request.Paging.PageNum,
+                    request.Paging.PageSize
+                };
+
+                object param2 = new
+                {
+                    request.TemplateID,
+                    UserID,
+                    request.ConditionFilter
                 };
 
                 var result = await _productRepository.spGetProductsByTemplateAndCondition(param);
                 var list = ((IEnumerable<dynamic>)result).ToList();
 
+                var pageData = await _productRepository.spGetProductsByTemplateAndConditionPageData(param2);
+                var dict = pageData as IDictionary<string, object>;
+                long count = 0;
+
+                if (dict != null && Int64.TryParse(dict["TotalRows"].ToString(), out _) == true)
+                {
+                    _ = Int64.TryParse(dict["TotalRows"].ToString(), out count);
+                }
+
                 if (list.Count > 0)
                 {
+                    long pageCount = count / request.Paging.PageSize;
+
                     response.StatusCode = "200";
                     response.Data = list;
                     response.Message = string.Empty;
+                    response.Paging = new PagingModel
+                    {
+                        PageNum = request.Paging.PageNum,
+                        PageSize = request.Paging.PageSize,
+                        PageCount = count % request.Paging.PageSize == 0 ? pageCount : pageCount + 1
+                    };
                 }
                 else
                 {
