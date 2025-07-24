72beb1a4f20d85a9208d7b1c4316fb66491bdf53
bachdoan-sa <bach.doansy@gmail.com>
2025-07-24 12:33:25 +0700

    Update DependencyInjection.cs

--- BG_IMPACT.Infrastructure/Extensions/DependencyInjection.cs
+++ BG_IMPACT.Infrastructure/Extensions/DependencyInjection.cs
@@ -24,6 +24,7 @@ namespace BG_IMPACT.Infrastructure.Extensions
             services.AddScoped<IProductGroupRepository, ProductGroupRepository>();
             services.AddScoped<IProductGroupRefRepository, ProductGroupRefRepository>();
             services.AddScoped<IProductTemplateRepository, ProductTemplateRepository>();
+            services.AddScoped<IStoreTableRepository, StoreTableRepository>();
             services.AddScoped<IBookListRepository, BookListRepository>();
             services.AddScoped<IBookItemRepository, BookItemRepository>();
             services.AddScoped<IOrderItemRepository, OrderItemRepository>();
