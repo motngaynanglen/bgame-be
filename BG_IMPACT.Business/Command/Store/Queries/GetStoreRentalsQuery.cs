65dbfc4bdbfcbeb9619e3d2906351fe7b2879d00
bachdoan-sa <bach.doansy@gmail.com>
2025-08-03 18:30:16 +0700

    add get rentals store

---
+++ BG_IMPACT.Business/Command/Store/Queries/GetStoreRentalsQuery.cs
@@ -0,0 +1,44 @@
+﻿namespace BG_IMPACT.Business.Command.Store.Queries
+{
+    public class GetStoreRentalsQuery : IRequest<ResponseObject>
+    {
+        public string Search { get; set; } = string.Empty;
+        public List<string> Filter { get; set; } = [];
+
+        public class GetStoreRentalsQueryHandler : IRequestHandler<GetStoreRentalsQuery, ResponseObject>
+        {
+            private readonly IStoreRepository _storeRepository;
+
+            public GetStoreRentalsQueryHandler(IStoreRepository storeRepository)
+            {
+                _storeRepository = storeRepository;
+            }
+            public async Task<ResponseObject> Handle(GetStoreRentalsQuery request, CancellationToken cancellationToken)
+            {
+                ResponseObject response = new();
+
+                object param = new
+                {
+                    request.Search
+                };
+
+                var result = await _storeRepository.spStoreGetRentals(param);
+                var list = ((IEnumerable<dynamic>)result).ToList();
+
+                if (list.Count > 0)
+                {
+                    response.StatusCode = "200";
+                    response.Data = list;
+                    response.Message = string.Empty;
+                }
+                else
+                {
+                    response.StatusCode = "404";
+                    response.Message = "Không tìm thấy cửa hàng nào.";
+                }
+
+                return response;
+            }
+        }
+    }
+}
--- BG_IMPACT/Controllers/StoreController.cs
+++ BG_IMPACT/Controllers/StoreController.cs
@@ -78,7 +78,34 @@ namespace BG_IMPACT.Controllers
                 return NotFound(new ResponseObject { StatusCode = "404", Message = "Chức năng đang bảo trì. Xin vui lòng thử lại sau!" });
             }
         }
-
+        [HttpPost("get-rentals")]
+        public async Task<IActionResult> GetRentals(GetStoreRentalsQuery command)
+        {
+            try
+            {
+                ResponseObject result = await _mediator.Send(command);
+                if (result.StatusCode == "200")
+                {
+                    return Ok(result);
+                }
+                else if (result.StatusCode == "403")
+                {
+                    return Forbid();
+                }
+                else if (result.StatusCode == "422")
+                {
+                    return UnprocessableEntity(result);
+                }
+                else
+                {
+                    return NotFound(result);
+                }
+            }
+            catch
+            {
+                return NotFound(new ResponseObject { StatusCode = "404", Message = "Chức năng đang bảo trì. Xin vui lòng thử lại sau!" });
+            }
+        }
         [HttpPost("get-list-by-group-ref-id")]
         public async Task<IActionResult> GetListByGroupRefId(GetStoreListByGroupRefIdQuery command)
         {
