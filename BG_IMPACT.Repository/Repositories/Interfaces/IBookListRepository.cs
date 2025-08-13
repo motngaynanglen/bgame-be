b6b834012dd6813fe48c90d0b7182ba1c0c6c0d5
PhamPhucThien <sherrin1st@gmail.com>
2025-08-13 11:35:01 +0700

    feat: Introduce `spBookListGetPaged` interface for paginated retrieval

--- BG_IMPACT.Repository/Repositories/Interfaces/IBookListRepository.cs
+++ BG_IMPACT.Repository/Repositories/Interfaces/IBookListRepository.cs
@@ -8,12 +8,13 @@ namespace BG_IMPACT.Repositories.Interfaces
         Task<object?> spBookListEnd(object param);
         Task<object?> spBookListGet(object param);
         Task<object?> spBookListExtend(object param);
-        Task<object?> spBookListGetPageData(object param);
+        Task<(object? bookLists, int totalCount)> spBookListGetPaged(object param);
         Task<object?> spBookListStart(object param);
         Task<object?> spBookListHistory(object param);
         Task<object?> spBookListHistoryPageData(object param);
         Task<object?> spBookListGetAvailableSlot(object param);
         Task<object?> spBookListGetAvailableProduct(object param);
         Task<object?> spBookListGetById(object param);
+        Task<object?> spBookListGetPageData(object param);
     }
 }
